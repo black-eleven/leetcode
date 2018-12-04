acc3d6c9cb496d4c83308176a959e7d613989fd6
yihuiwen <yihuiwen@hinterstellar.com>
2018-12-05 00:27:21 +0800

    solve 769

---
+++ algorithms/769/max_chunks_to_make_sorted.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def maxChunksToSorted(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        i = 0
+        ret = 1
+        for j, element in enumerate(arr):
+            if element > arr[i]:
+                if arr[i] == j-1:
+                    ret += 1
+                i = j
+        return ret
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.maxChunksToSorted([4,3,2,1,0]) # 1
+    print s.maxChunksToSorted([1,0,2,3,4]) # 4
+    print s.maxChunksToSorted([1,2,0,3]) # 2
+    print s.maxChunksToSorted([0, 2, 1]) # 2
+    print s.maxChunksToSorted([0]) # 1
+    print s.maxChunksToSorted([0, 1]) # 2

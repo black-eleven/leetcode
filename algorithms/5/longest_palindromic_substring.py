c58ab09daae67daf8f3bb00fdbea618b558397c2
yihuiwen <yihuiwen@hinterstellar.com>
2019-03-30 23:16:44 +0800

    solve 5

---
+++ algorithms/5/longest_palindromic_substring.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+
+
+class Solution(object):
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
